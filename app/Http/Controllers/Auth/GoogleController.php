d08d337c224f8f11866f5e083745e2889dd7a9cc
MaicolGARCIA1 <115556588+MaicolGARCIA1@users.noreply.github.com>
2024-05-07 10:08:49 -0500

    Cambios en las interacciones

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+use App\Models\User; // Asegúrate de importar el modelo User si aún no lo has hecho
 use Auth;
 
 class GoogleController extends Controller
@@ -14,16 +16,32 @@ class GoogleController extends Controller
     }
 
     public function handleGoogleCallback(Request $request)
-{
-    try {
-        $user = Socialite::driver('google')->user();
-    } catch (\Laravel\Socialite\Two\InvalidStateException $e) {
-        // Manejar la excepción, por ejemplo, redirigir al usuario a una página de error.
-        return redirect()->route('error')->with('message', 'Hubo un problema con la autenticación. Por favor, intenta nuevamente.');
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+        }catch (\Laravel\Socialite\Two\InvalidStateException $e) {
+            return redirect()->route('error')->with('message', 'Hubo un problema con la autenticación de Google. Por favor, intenta nuevamente.');
+        }
+        
+
+        // Verificar si el usuario ya existe en la base de datos
+        $existingUser = user::where('email', $user->email)->first();
+
+        if ($existingUser) {
+            // Si el usuario ya existe, iniciar sesión en la aplicación
+            Auth::login($existingUser);
+        } else {
+            // Si el usuario no existe, crear uno nuevo en la base de datos
+            $newUser = new user();
+            $newUser->name = $user->name;
+            $newUser->email = $user->email;
+            $newUser->save();
+
+            // Iniciar sesión con el nuevo usuario
+            Auth::login($newUser);
+        }
+
+        // Redirigir a la página de inicio después de iniciar sesión
+        return redirect()->route('home');
     }
-
-    // Resto de tu lógica aquí...
-}
-
-
 }
